45f12ea7fa080909187297714ef964dd309c0e93
Sohan Karfa <sohan100karfa@gmail.com>
2025-07-21 13:27:33 +0000

    make the UI responsive, user should not need to scroll horizontally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             <BackgroundStars />
             <div className="flex flex-col min-h-screen">
               <SiteHeader />
-              <main className="flex-1 p-4 md:p-8 z-10">
+              <main className="flex-1 px-4 md:px-8 z-10">
                 {children}
               </main>
             </div>
